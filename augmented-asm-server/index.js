d11f2578d5110bbf0fa965815d41ff09b5755c22
George Edwards <george.edwards@csiro.au>
2021-06-15 14:00:38 +1000

    added ip+hostname to analytics (RESTful server). Removed obsolete versions

--- augmented-asm-server/index.js
+++ augmented-asm-server/index.js
@@ -1,27 +1,50 @@
 const filenameLog = "C:\\Users\\edw19b\\Dropbox\\dev\\augmented-asm\\augmented-asm-server\\log.json";
 const filenameLogUnique = "C:\\Users\\edw19b\\Dropbox\\dev\\augmented-asm\\augmented-asm-server\\log-unique.json";
-const fs = require('fs');
 const express = require('express');
+const fs = require('fs');
+const dns = require('dns');
+const util = require('util');
+const lookup = util.promisify(dns.reverse);
 const app = express();
 const PORT = 8080;
 
 app.use( express.json() ); // middleware
 app.listen(PORT, () => console.log(`running on port ${PORT}`))
 
+async function getHostname(ip) {
+    try {
+        return await lookup(ip);
+    } catch (err) {
+        throw err;
+    }
+};
+
 app.post('/wave-hello', (req, res) => {
     // load
     let logData = JSON.parse(fs.readFileSync(filenameLog));
     let logDataUnique = JSON.parse(fs.readFileSync(filenameLogUnique));
-    
-    // append entry
-    logData.users.push(req.body);
+    let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // get incoming IP from request headers
+
+    // append incoming data with IP (sync) and Hostname (async) then once promise has resolved write the file to disk.
+    req.body.ip = ip;
+    getHostname(ip)
+        .then(value => {
+            req.body.hostname = value[0];
+            logData.users.push(req.body);
+            fs.writeFileSync(filenameLog, JSON.stringify(logData, null, 2));
+        })
+        
+        .catch(error => {
+            req.body.hostname = `lookup error: ${error.errno}`;
+            logData.users.push(req.body);
+            fs.writeFileSync(filenameLog, JSON.stringify(logData, null, 2));
+        })
 
     // append unique entry
     if (!logDataUnique.users.find(e => e.user === req.body.user))
         logDataUnique.users.push(req.body);
     
     // write
-    fs.writeFileSync(filenameLog, JSON.stringify(logData, null, 2))
     fs.writeFileSync(filenameLogUnique, JSON.stringify(logDataUnique, null, 2))
 
     // send response
@@ -42,3 +65,4 @@ app.get('/:other', (req, res) => {
     // send response
     res.send( `what's this now? ☝️` );
 })
+
