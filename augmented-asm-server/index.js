0706a8d749bcd7f5bc79d3d8295326931f06e2b4
George Edwards <george.edwards@csiro.au>
2021-07-04 19:02:24 +1000

    updated version number on server

--- augmented-asm-server/index.js
+++ augmented-asm-server/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const dns = require('dns');
 const app = express();
 const PORT = 8080;
-const VERSION = 1.50;
+const VERSION = 1.51;
 
 app.use( express.json() ); // load json middleware
 
